29e39ef4c9c54faddc445fb44bb692d8ed5f80d6
Frederick Pringle <freddyjepringle@gmail.com>
2024-04-12 15:14:13 +0200

    Build `servant-routes` and `servant-routes-doc` as separate derivations

--- default.nix
+++ default.nix
@@ -1,1 +1,6 @@
-import ./servant-routes.nix
+args@{ compiler ? "ghc928" }:
+let pkg = import ./servant-routes.nix args;
+in {
+  inherit (pkg) servant-routes;
+  inherit (pkg.servant-routes) doc;
+}
