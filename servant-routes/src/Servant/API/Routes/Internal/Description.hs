c3fb257be1fd0cc07a6ba98ba47d71c47b05da53
Frederick Pringle <frederick.pringle@fpringle.com>
2025-11-03 18:25:23 +0100

    Move `RouteDescription` to its own module

---
+++ servant-routes/src/Servant/API/Routes/Internal/Description.hs
@@ -0,0 +1,40 @@
+{- |
+Module      : Servant.API.Routes.Internal.Description
+Copyright   : (c) Frederick Pringle, 2025
+License     : BSD-3-Clause
+Maintainer  : frederick.pringle@fpringle.com
+
+Internal module, subject to change.
+-}
+module Servant.API.Routes.Internal.Description
+  ( RouteDescription (..)
+  )
+where
+
+import Data.Aeson
+import Data.String (IsString)
+import qualified Data.Text as T
+
+{- | Description of a route. This will correspond to the Servant @Description@ combinator.
+
+It should  be noted that the 'HasRoutes' behaviour for @Description@ diverges from that in
+@servant-openapi3@, in the case that one EP has multiple @Description@ combinators.
+For example, given the following API:
+
+@
+type MyAPI =
+  "transaction" :> TransactionAPI
+    :\<|> "user" :> Description "User sub-API"
+          :> ( Description "Get my user" :> Get '[JSON] User
+              :\<|> "list" :> Get '[JSON] [User]
+             )
+@
+
+The @Operation@ that @servant-openapi@ generates for the @GET /user@ endpoint will have the two
+@Description@s 'mappend'-ed together: @"User sub-APIGet my user"@.
+
+The corresponding 'Route' will take the most specific 'RouteDescription': @"Get my user"@.
+-}
+newtype RouteDescription = RouteDescription {unDescription :: T.Text}
+  deriving (Show)
+  deriving (Eq, IsString, Ord, Semigroup, Monoid, ToJSON, FromJSON) via T.Text
--- servant-routes/src/Servant/API/Routes/Internal/Route.hs
+++ servant-routes/src/Servant/API/Routes/Internal/Route.hs
@@ -37,34 +37,11 @@ import Lens.Micro.TH
 import Network.HTTP.Types.Method (Method)
 import "this" Servant.API.Routes.Auth
 import "this" Servant.API.Routes.Header
+import "this" Servant.API.Routes.Internal.Description
 import "this" Servant.API.Routes.Internal.Request
 import "this" Servant.API.Routes.Internal.Response
 import "this" Servant.API.Routes.Param
 import "this" Servant.API.Routes.Path
-
-{- | Description of a route. This will correspond to the Servant @Description@ combinator.
-
-It should  be noted that the 'HasRoutes' behaviour for @Description@ diverges from that in
-@servant-openapi3@, in the case that one EP has multiple @Description@ combinators.
-For example, given the following API:
-
-@
-type MyAPI =
-  "transaction" :> TransactionAPI
-    :\<|> "user" :> Description "User sub-API"
-          :> ( Description "Get my user" :> Get '[JSON] User
-              :\<|> "list" :> Get '[JSON] [User]
-             )
-@
-
-The @Operation@ that @servant-openapi@ generates for the @GET /user@ endpoint will have the two
-@Description@s 'mappend'-ed together: @"User sub-APIGet my user"@.
-
-The corresponding 'Route' will take the most specific 'RouteDescription': @"Get my user"@.
--}
-newtype RouteDescription = RouteDescription {unDescription :: T.Text}
-  deriving (Show)
-  deriving (Eq, IsString, Ord, Semigroup, Monoid, ToJSON, FromJSON) via T.Text
 
 {- | Short summary of a route. This will correspond to the Servant @Summary@ combinator.
 The behaviour described for 'RouteDescription' is the same for 'RouteSummary'.
